ed9ef2a2810b11c3d766a8ddc8db296f913e173e
Yurii Dvornichenko <raen98@live.ru>
2022-04-12 21:13:24 +0300

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,7 +4,6 @@ const galleryEl = document.querySelector(".gallery");
 const makeTransactionRole = makeGalleryPicture(galleryItems);
 
 galleryEl.insertAdjacentHTML("afterbegin", makeTransactionRole);
-// console.log(galleryItems);
 
 galleryEl.addEventListener("click", onGallaryConteinerClick);
 
@@ -27,31 +26,8 @@ function onGallaryConteinerClick(e) {
     return;
   }
 
-  console.log(e.target.alt);
-  const nextActivPicture = e.target.dataset.source;
-  //   onOpenModal(nextActivPicture);
+  new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    animationSpeed: 250,
+  });
 }
-
-// function onOpenModal(nextActivPicture) {
-//   window.addEventListener("keydown", onEscKeyPress);
-
-//   const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <img src=${nextActivPicture} width="1120" height="100%">
-//     </div>
-// `);
-
-//   instance.show();
-// }
-
-// function onEscKeyPress(e) {
-//   console.log(e.code);
-//   if (e.code === "Escape") {
-//     onCloseModal();
-//   }
-// }
-
-// function onCloseModal() {
-//   window.removeEventListener("keydown", onEscKeyPress);
-//   // instance.close();
-// }
